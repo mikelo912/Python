bfdd4edb84b4970deab68ad292d2bf570d9540c7
mikelo912 <128706988+mikelo912@users.noreply.github.com>
2023-03-23 21:42:47 +0800

    Add files via upload

---
+++ 0323/ex04.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 23 21:19:15 2023
+
+@author: USER
+"""
+
+number = [10,20,30,40,50]
+number.insert(2,99)
+print(number)
+
+n = number.index(40)
+print(n)
+
+count = number.count(200)
+print("數量:",count)
