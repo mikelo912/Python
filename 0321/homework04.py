bc97f718cfad703a3e6e6a467de8aaac33be021b
mikelo912 <128706988+mikelo912@users.noreply.github.com>
2023-03-28 19:18:14 +0800

    Update homework04.py

--- 0321/homework04.py
+++ 0321/homework04.py
@@ -22,3 +22,17 @@ elif a == c and a !=b:
     print("此三角形為等腰三角形")
 elif a == b and a == c:
     print("此三角形為正三角形")   
+    
+a = int(input("請輸入第一個數字:"))
+b = int(input("請輸入第二個數字:"))
+c = int(input("請輸入第三個數字:"))
+
+if a+b > c or b+c > a or c+a >b:
+    print("這三個數字可以構成三角形")
+else:
+    print("這三個數字不能構成三角形")
+    
+if a == b == c:
+    print("此三角形為正三角形")
+    elif a==b or a==c or b==c:
+        print("此三角形為等腰三角形")
