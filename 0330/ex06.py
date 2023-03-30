2087c54e06bfd7bdb5a20a6f5ff31bdd1076d6a8
mikelo912 <128706988+mikelo912@users.noreply.github.com>
2023-03-30 21:49:53 +0800

    Add files via upload

---
+++ 0330/ex06.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 30 21:38:14 2023
+
+@author: USER
+"""
+
+fruit = {"apple": 34,"orange":68, "Banana":49}
+fruit["orange"] = 168
+print(fruit)
+del fruit["apple"]
+print(fruit)
+fruit.clear()
+print(fruit)
+
+data = [80,60,70,90,49,33,89]
+'''
+第一題
+請排序由小至大，用sort()
+第二題
+請輸入數字去搜尋，要顯示有找到或沒有找到;
+有找到的要顯示你找了幾次
+'''
