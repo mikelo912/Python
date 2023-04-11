36c950b6431c84186beb43cb544c02963414b046
mikelo912 <128706988+mikelo912@users.noreply.github.com>
2023-04-11 18:57:38 +0800

    Update homework01.py

--- 0406/homework01.py
+++ 0406/homework01.py
@@ -5,13 +5,14 @@ Spyder Editor
 This is a temporary script file.
 """
 
+#巴斯卡三角形
 n = int(input("幾層?"))
 data = [1]
 for i in range(n):
-    print(data)
+    print(data) #先印出data串列
     plist = []
-    plist.append(data[0])
+    plist.append(data[0]) 
     for i in range(len(data)-1):
-        plist.append(data[i]+data[i+1])
-    plist.append(data[-1])
+        plist.append(data[i]+data[i+1]) #新增data[i]+data[i+1]到plist串列
+    plist.append(data[-1]) #抓data串列的最後一個值
     data = plist
