afcbe218f46ee8bde9612a699e0af2a772b531de
mikelo912 <128706988+mikelo912@users.noreply.github.com>
2023-04-11 12:01:46 +0800

    Update ex01.py

--- 0328/ex01.py
+++ 0328/ex01.py
@@ -9,7 +9,7 @@ scores = [60,70,65,100,91,88,65,81]
 search = 65
 start = 0
 
-for i in range(scores.count(search)):
-    index = scores.index(search,start+1)
+for i in range(scores.count(search)): #計算search的值在scores裡有幾個
+    index = scores.index(search,start+1) #使用index()方法查找scores列表中從start+1位置開始的第一個匹配項的索引
     print("索引值:",index)
     start = index + 1
