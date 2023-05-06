221515c9cc7842d3284185c7eba227771071dcbb
mikelo912 <128706988+mikelo912@users.noreply.github.com>
2023-05-06 19:28:45 +0800

    Update ex09.py

--- 0504/ex09.py
+++ 0504/ex09.py
@@ -8,7 +8,7 @@ Created on Thu May  4 20:30:32 2023
 fn = "file02.txt"
 with open(fn,encoding='utf-8') as f:
     data = f.read()
-    newdata = data.replace('單身受薪','蔡依林')
+    newdata = data.replace('單身受薪','蔡依林') #用後面的字串取代前面的字串
     print(newdata.strip())
     
 with open("news.txt",'w',encoding='UTF-8') as f:
