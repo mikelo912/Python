6e57ad265e6a27b4245c63aff3ad66fefaef1086
mikelo912 <the.whims.of.fate2021@gmail.com>
2023-05-22 23:03:52 +0800

    0522更新

---
+++ 0321-第 2 堂 判斷式與迴圈/homework01.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+#作業1
+for a in range(1,10):
+    for b in range(2,10):
+        print(b,"*",a,"=",b*a,sep="",end="\t")
+    print()
